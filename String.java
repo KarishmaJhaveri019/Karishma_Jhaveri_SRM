a78c7519a009ab9af8b9266de185deb8023a5dea
KarishmaJhaveri019 <95085648+KarishmaJhaveri019@users.noreply.github.com>
2023-01-25 10:48:50 +0530

    Update String.java

--- String.java
+++ String.java
@@ -1,5 +1,7 @@
-    public class String{  
-     	public static void main(String args[]){  
-    	String s1="This is Karishma Jhaveri";   
-    	System.out.println("string length is: "+s1.length());//10 is the length of javatpoint string  
-    }}  
+public class String {
+  public static void main(String[] args) {
+    String str = "Hello, Karishma!";
+    int length = str.length();
+    System.out.println("The length of the string is: " + length);
+  }
+}
