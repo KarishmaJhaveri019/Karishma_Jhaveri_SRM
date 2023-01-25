492f172eb3166baab8449dcee64f2f64f571ae9d
KarishmaJhaveri019 <95085648+KarishmaJhaveri019@users.noreply.github.com>
2023-01-25 10:40:03 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,7 @@
+public class String {
+	    public static void main(String[] args) {
+	        String str = "Karishma Jhaveri";
+	        int length = str.length();
+	        System.out.println("Length of the string: " + length);
+	    }
+	}
